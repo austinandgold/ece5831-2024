d89d7f7c397240b2e651014a90facd3c22dc40da
Austinandgold <austinmo@umich.edu>
2024-09-28 08:48:10 -0400

    finished logic gate functionality

--- 03/logicgate.py
+++ 03/logicgate.py
@@ -7,14 +7,61 @@ class LogicGate():
         pass
 
     def andgate (self,x1,x2):
-        w = np.array([0.5, 0.5, 1])          
+        w = np.array([1, 1, 1])          
+        b = -1.9
+        X = np.array([x1, x2, b])
+
+        y = np.sum(w*X)
+
+        if y > 0:
+            return 1
+        else:
+            return 0
+        
+    def orgate (self,x1,x2):
+        w = np.array([1, 1, 1])          
         b = -0.9
         X = np.array([x1, x2, b])
 
-        y = sum(w*X)
+        y = np.sum(w*X)
 
         if y > 0:
             return 1
         else:
             return 0
 
+    def nandgate (self,x1,x2):
+        w = np.array([-1, -1, 1])          
+        b = 1.9
+        X = np.array([x1, x2, b])
+
+        y = np.sum(w*X)
+
+        if y > 0:
+            return 1
+        else:
+            return 0
+        
+    def norgate (self,x1,x2):
+        w = np.array([-1, -1, 1])          
+        b = 0.9
+        X = np.array([x1, x2, b])
+
+        y = np.sum(w*X)
+
+        if y > 0:
+            return 1
+        else:
+            return 0
+        
+    def xorgate (self,x1,x2):
+        w = np.array([0.5, 0.5, 1])          
+        b = -0.5
+        X = np.array([x1, x2, b])
+
+        y = np.sum(w*X)
+
+        if y == 0:
+            return 1
+        else:
+            return 0
