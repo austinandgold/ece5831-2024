d8c1c30acb5a71a7ece7d72e80d6b72d818a1c4b
Austinandgold <austinmo@umich.edu>
2024-10-06 11:13:53 -0400

    Just got the ipynb version working

--- 04/multilayer_perceptron.py
+++ 04/multilayer_perceptron.py
@@ -22,6 +22,9 @@ class MultiLayerPreceptron:
     def sigmoid(self,x):
         return 1/(1 + np.exp(-x))
 
+    def identity(self, x):
+        return x
+
     def forward(self,x):
         w1, w2, w3 = self.net['w1'], self.net['w2'], self.net['w3']
         b1, b2, b3 = self.net['b1'], self.net['b2'], self.net['b3']
@@ -29,13 +32,11 @@ class MultiLayerPreceptron:
         a1 = np.dot(x, w1) + b1
         z1 = self.sigmoid(a1)
 
-        a2 = np.dot(x, w2) + b2
+        a2 = np.dot(z1, w2) + b2
         z2 = self.sigmoid(a2)
 
-        a3 = np.dot(x, w3) + b3
-        y = self.sigmoid(a3)
+        a3 = np.dot(z2, w3) + b3
+        y = self.identity(a3)
 
         return y
     
-    def identity(self, x):
-        return x
